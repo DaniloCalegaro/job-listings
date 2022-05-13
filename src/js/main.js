96cdc12ff65352bf49a0f44a91a5a796a6b47fac
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-05-13 14:26:29 -0300

    feat(conection) - conexão com json

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,60 @@
-console.log('-------')
+//console.log('-------')
+
+/*----Received Json----*/
+const pathJson = './src/json/data.json'
+
+fetch(pathJson)
+  .then(response => {
+    return response.json()
+  })
+  .then(jsondata => receivedJsonData(jsondata))
+
+/*----Create element----*/
+function newElement(name, className, type) {
+  name = document.createElement(type)
+  name.className = className
+  return name
+}
+
+/*----Insert Dynamic---- */
+const pageMain = document.querySelector('main')
+
+function receivedJsonData(jsonData) {
+  //console.log(jsonData[0])
+  jsonData.forEach(element => {
+    container = newElement('container', 'container', 'div')
+
+    company = newElement('company', 'company', 'div')
+
+    filters = newElement('filters', 'filters', 'div')
+    listFilters = newElement('listFilters', '', 'ul')
+
+    const rowListContract = newElement('rowListContract', '', 'li')
+    const contract = newElement('contract', 'button-filters', 'a')
+    contract.textContent = element.contract
+
+    rowListContract.appendChild(contract)
+    listFilters.appendChild(rowListContract)
+
+    const rowListLocation = newElement('rowListLocation', '', 'li')
+    const location = newElement('location', 'button-filters', 'a')
+    location.textContent = element.location
+
+    rowListLocation.appendChild(location)
+    listFilters.appendChild(rowListLocation)
+
+    element.languages.forEach(e => {
+      const rowListLanguage = newElement('rowListLanguage', '', 'li')
+      const language = newElement('language', 'button-filters', 'a')
+      language.textContent = e
+
+      rowListLanguage.appendChild(language)
+      listFilters.appendChild(rowListLanguage)
+    })
+
+    filters.appendChild(listFilters)
+    container.appendChild(company)
+    container.appendChild(filters)
+    pageMain.appendChild(container)
+  })
+}
