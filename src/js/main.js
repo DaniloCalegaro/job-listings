0925d09889fc9fc2132eb8facd6e7b7a6768585d
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-05-19 12:44:30 -0300

    refactor(main): melhorando sintaxe do codigo

--- src/js/main.js
+++ src/js/main.js
@@ -3,174 +3,201 @@
 /*----Received Json----*/
 const pathJson = './src/json/data.json'
 
-function fetchJson(filtering) {
+function fetchJson() {
   fetch(pathJson)
     .then(response => {
       return response.json()
     })
-    .then(jsondata => receivedJsonData(jsondata, filtering))
+    .then(jsondata => receivedJsonData(jsondata))
 }
+
+/*--- Fetch Filtering ---*/
+function fetchJsonFiltering(filter) {
+  fetch(pathJson)
+    .then(response => {
+      return response.json()
+    })
+    .then(jsondata => {
+      const jsonFiltering = jsondata.filter(e => e.contract === filter)
+      console.log(jsonFiltering)
+      // jsonFiltering.forEach(element => {
+      //   if (checkRepeatedItem(element, itensFiltersSelected))
+      //     itensFiltersSelected.push(element.id)
+      // })
+      //console.log(jsondata[1].contract)
+    })
+}
+fetchJsonFiltering('Full Time')
 
 /*----Arrays----*/
 let typeContracts = []
 let locations = []
 let filtersSelected = []
 
-function receivedJsonData(jsonData, filtering) {
+const pageMain = document.querySelector('main')
+
+function receivedJsonData(jsonData) {
   /*----Insert Dynamic---- */
+  jsonData.forEach(element => {
+    divContainer = newElement('container', 'container', 'section')
 
-  const pageMain = document.querySelector('main')
-  if (filtering) {
-    pageMain.remove()
-  } else {
-    jsonData.forEach(element => {
-      divContainer = newElement('container', 'container', 'section')
+    //Company
+    divCompany = newElement('company', 'company', 'div')
 
-      //Company
-      divCompany = newElement('company', 'company', 'div')
+    divImageCompany = newElement('imageCompany', 'image-company', 'div')
+    imgCompany = newElement('imgCompany', '', 'img')
+    imgCompany.src = element.logo
 
-      divImageCompany = newElement('imageCompany', 'image-company', 'div')
-      imgCompany = newElement('imgCompany', '', 'img')
-      imgCompany.src = element.logo
+    divImageCompany.appendChild(imgCompany)
+    divCompany.appendChild(divImageCompany)
 
-      divImageCompany.appendChild(imgCompany)
-      divCompany.appendChild(divImageCompany)
-
-      divDetailsCompany = newElement(
-        'divDetailsCompany',
-        'details-company',
-        'div'
-      )
-      divCompanyNameFeature = newElement(
-        'divCompanyNameFeature',
-        'company-name-feature',
-        'div'
-      )
-      listCompanyNameFeature = newElement('listCompanyNameFeature', '', 'ul')
-      rowCompany = newElement('rowCompany', '', 'li')
-      companyName = newElement('companyName', '', 'p')
-      companyName.textContent = element.company
-      rowNew = newElement('rowNew', '', 'li')
-      companyNew = newElement('companyNew', 'button-new', 'p')
-      companyNew.textContent = element.new
-      rowFeatured = newElement('rowFeatured', '', 'li')
-      companyFeatured = newElement('companyFeatured', 'button-featured', 'p')
-      companyFeatured.textContent = element.featured
-
-      rowCompany.appendChild(companyName)
-      listCompanyNameFeature.appendChild(rowCompany)
-
-      rowNew.appendChild(companyNew)
-      listCompanyNameFeature.appendChild(rowNew)
-
-      rowFeatured.appendChild(companyFeatured)
-      listCompanyNameFeature.appendChild(rowFeatured)
-
-      divCompanyNameFeature.appendChild(listCompanyNameFeature)
-
-      divJob = newElement('divJob', 'job', 'div')
-      positionJob = newElement('positionJob', '', 'p')
-      positionJob.textContent = element.position
-
-      divJob.appendChild(positionJob)
-
-      divDetailsJob = newElement('divDetailsJob', 'details-job', 'div')
-      listDetailsJob = newElement('listDetailsJob', '', 'ul')
-
-      rowPostedAt = newElement('rowPostedAt', '', 'li')
-      postedAt = newElement('postedAt', '', 'p')
-      postedAt.textContent = element.postedAt
-      rowPostedAt.appendChild(postedAt)
-
-      rowContract = newElement('rowContract', '', 'li')
-      contractJob = newElement('contractJob', '', 'p')
-      contractJob.textContent = element.contract
-      rowContract.appendChild(contractJob)
-
-      rowLocation = newElement('rowLocation', '', 'li')
-      locationJob = newElement('locationJob', '', 'p')
-      locationJob.textContent = element.location
-      rowLocation.appendChild(locationJob)
-
-      listDetailsJob.appendChild(rowPostedAt)
-      listDetailsJob.appendChild(rowContract)
-      listDetailsJob.appendChild(rowLocation)
-      divDetailsJob.appendChild(listDetailsJob)
-
-      divDetailsCompany.appendChild(divCompanyNameFeature)
-      divDetailsCompany.appendChild(divJob)
-      divDetailsCompany.appendChild(divDetailsJob)
-
-      divCompany.appendChild(divDetailsCompany)
-      //------
-
-      //Results Json
-      const spanResult = document.querySelector('#cont-result')
-      spanResult.textContent = jsonData.length
-
-      //Filters
-
-      divFilters = newElement('filters', 'filters', 'div')
-      listFilters = newElement('.listFilters', '', 'ul')
-
-      const rowListContract = newElement('rowListContract', '', 'li')
-      const contract = newElement('contract', 'button-filters', 'a')
-      contract.textContent = element.contract
-
-      rowListContract.appendChild(contract)
-      listFilters.appendChild(rowListContract)
-
-      const rowListLocation = newElement('rowListLocation', '', 'li')
-      const location = newElement('location', 'button-filters', 'a')
-      location.textContent = element.location
-
-      rowListLocation.appendChild(location)
-      listFilters.appendChild(rowListLocation)
-
-      element.languages.forEach(e => {
-        const rowListLanguage = newElement('rowListLanguage', '', 'li')
-        const language = newElement('language', 'button-filters', 'a')
-        language.textContent = e
-
-        rowListLanguage.appendChild(language)
-        listFilters.appendChild(rowListLanguage)
-      })
-
-      divFilters.appendChild(listFilters)
-      //--------
-      divContainer.appendChild(divCompany)
-      divContainer.appendChild(divFilters)
-      pageMain.appendChild(divContainer)
-
-      // Add contracts and locations array
-      if (checkRepeatedItem(element.contract, typeContracts))
-        typeContracts.push(element.contract)
-
-      if (checkRepeatedItem(element.location, locations))
-        locations.push(element.location)
-    })
-
-    /*----Filters Modal Dinamics---- */
-    const listDinamicModalFilters = document.querySelector(
-      '#dinamic-modal-filters'
+    divDetailsCompany = newElement(
+      'divDetailsCompany',
+      'details-company',
+      'div'
     )
+    divCompanyNameFeature = newElement(
+      'divCompanyNameFeature',
+      'company-name-feature',
+      'div'
+    )
+    listCompanyNameFeature = newElement('listCompanyNameFeature', '', 'ul')
+    rowCompany = newElement('rowCompany', '', 'li')
+    companyName = newElement('companyName', '', 'p')
+    companyName.textContent = element.company
+    rowNew = newElement('rowNew', '', 'li')
+    companyNew = newElement('companyNew', 'button-new', 'p')
+    companyNew.textContent = element.new
+    rowFeatured = newElement('rowFeatured', '', 'li')
+    companyFeatured = newElement('companyFeatured', 'button-featured', 'p')
+    companyFeatured.textContent = element.featured
 
-    typeContracts.forEach(e => {
-      const rowTypeContract = newElement('rowTypeContract', '', 'li')
-      rowTypeContract.textContent = e
-      listDinamicModalFilters.appendChild(rowTypeContract)
+    rowCompany.appendChild(companyName)
+    listCompanyNameFeature.appendChild(rowCompany)
+
+    rowNew.appendChild(companyNew)
+    listCompanyNameFeature.appendChild(rowNew)
+
+    rowFeatured.appendChild(companyFeatured)
+    listCompanyNameFeature.appendChild(rowFeatured)
+
+    divCompanyNameFeature.appendChild(listCompanyNameFeature)
+
+    divJob = newElement('divJob', 'job', 'div')
+    positionJob = newElement('positionJob', '', 'p')
+    positionJob.textContent = element.position
+
+    divJob.appendChild(positionJob)
+
+    divDetailsJob = newElement('divDetailsJob', 'details-job', 'div')
+    listDetailsJob = newElement('listDetailsJob', '', 'ul')
+
+    rowPostedAt = newElement('rowPostedAt', '', 'li')
+    postedAt = newElement('postedAt', '', 'p')
+    postedAt.textContent = element.postedAt
+    rowPostedAt.appendChild(postedAt)
+
+    rowContract = newElement('rowContract', '', 'li')
+    contractJob = newElement('contractJob', '', 'p')
+    contractJob.textContent = element.contract
+    rowContract.appendChild(contractJob)
+
+    rowLocation = newElement('rowLocation', '', 'li')
+    locationJob = newElement('locationJob', '', 'p')
+    locationJob.textContent = element.location
+    rowLocation.appendChild(locationJob)
+
+    listDetailsJob.appendChild(rowPostedAt)
+    listDetailsJob.appendChild(rowContract)
+    listDetailsJob.appendChild(rowLocation)
+    divDetailsJob.appendChild(listDetailsJob)
+
+    divDetailsCompany.appendChild(divCompanyNameFeature)
+    divDetailsCompany.appendChild(divJob)
+    divDetailsCompany.appendChild(divDetailsJob)
+
+    divCompany.appendChild(divDetailsCompany)
+    //------
+
+    //Results Json
+    const spanResult = document.querySelector('#cont-result')
+    spanResult.textContent = jsonData.length
+
+    //Filters
+
+    divFilters = newElement('filters', 'filters', 'div')
+    listFilters = newElement('.listFilters', '', 'ul')
+
+    const rowListContract = newElement('rowListContract', '', 'li')
+    const contract = newElement('contract', 'button-filters', 'a')
+    contract.textContent = element.contract
+
+    rowListContract.appendChild(contract)
+    listFilters.appendChild(rowListContract)
+
+    const rowListLocation = newElement('rowListLocation', '', 'li')
+    const location = newElement('location', 'button-filters', 'a')
+    location.textContent = element.location
+
+    rowListLocation.appendChild(location)
+    listFilters.appendChild(rowListLocation)
+
+    element.languages.forEach(e => {
+      const rowListLanguage = newElement('rowListLanguage', '', 'li')
+      const language = newElement('language', 'button-filters', 'a')
+      language.textContent = e
+
+      rowListLanguage.appendChild(language)
+      listFilters.appendChild(rowListLanguage)
     })
 
-    const rowDivider = newElement('rowDivider', 'list-break', 'li')
-    listDinamicModalFilters.appendChild(rowDivider)
+    divFilters.appendChild(listFilters)
+    //--------
+    divContainer.appendChild(divCompany)
+    divContainer.appendChild(divFilters)
+    pageMain.appendChild(divContainer)
 
-    locations.forEach(e => {
-      const rowLocation = newElement('rowLocation', '', 'li')
-      rowLocation.textContent = e
-      listDinamicModalFilters.appendChild(rowLocation)
-    })
-  }
+    // Add contracts and locations array filters
+    if (!checkRepeatedItem(element.contract, typeContracts))
+      typeContracts.push(element.contract)
+
+    if (!checkRepeatedItem(element.location, locations))
+      locations.push(element.location)
+  })
+
+  /*----Filters Modal Dinamics---- */
+  const listDinamicModalFilters = document.querySelector(
+    '#dinamic-modal-filters'
+  )
+
+  typeContracts.forEach(e => {
+    const rowTypeContract = newElement('rowTypeContract', '', 'li')
+    rowTypeContract.textContent = e
+    listDinamicModalFilters.appendChild(rowTypeContract)
+  })
+
+  const rowDivider = newElement('rowDivider', 'list-break', 'li')
+  listDinamicModalFilters.appendChild(rowDivider)
+
+  locations.forEach(e => {
+    const rowLocation = newElement('rowLocation', '', 'li')
+    rowLocation.textContent = e
+    listDinamicModalFilters.appendChild(rowLocation)
+  })
 }
+
+/*---- EventListeners Modal---- */
+
+const filtersView = document.querySelector('#filters-view')
+
+filtersView.addEventListener('click', () => {
+  modalFilters.classList.toggle('active')
+})
+
+const modalFilters = document.querySelector('#modal-filters')
+modalFilters.addEventListener('click', () => {
+  modalFilters.classList.toggle('active')
+})
 
 /*--- Add Button List Dinamic Filters Header ---*/
 const listDinamicModalFilters = document.querySelector('#dinamic-modal-filters')
@@ -186,7 +213,7 @@ listDinamicModalFilters.addEventListener('click', e => {
 
   const valueElementClick = e.target.firstChild.textContent
 
-  if (checkRepeatedItem(valueElementClick, filtersSelected)) {
+  if (!checkRepeatedItem(valueElementClick, filtersSelected)) {
     filtersSelected.push(valueElementClick)
     filterSelected.textContent = e.target.firstChild.textContent
     rowFilterSelected.id = e.target.firstChild.textContent
@@ -216,23 +243,9 @@ function newElement(name, className, type) {
   return name
 }
 
-const checkRepeatedItem = (element, array) =>
-  array.indexOf(element) === -1 ? true : false
+const checkRepeatedItem = (element, array) => array.some(e => e === element)
 
-/*---- EventListeners Modal---- */
-
-const filtersView = document.querySelector('#filters-view')
-
-filtersView.addEventListener('click', () => {
-  modalFilters.classList.toggle('active')
-})
-
-const modalFilters = document.querySelector('#modal-filters')
-modalFilters.addEventListener('click', () => {
-  modalFilters.classList.toggle('active')
-})
-
-/*--- Initialize `page ---*/
+/*--- Initialize page ---*/
 
 window.onload = () => {
   fetchJson()
